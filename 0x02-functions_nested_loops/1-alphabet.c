a8bc558d9c71002d873ea1d144b973e0790e0a29
Enebeli Chukwuemeka <enebelichukwuemeka999@gmail.com>
2022-09-13 15:53:25 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point 
+ *
+ * print_alphabet - print alphabet lowercase
+ * Return: always 0
+ *
+ */
+void print_alphabet(void)
+{
+	char alpabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}

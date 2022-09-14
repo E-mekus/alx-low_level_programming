73d7d68b61a80259ab7790b5046969dba0952645
Enebeli Chukwuemeka <enebelichukwuemeka999@gmail.com>
2022-09-14 15:12:52 +0000

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * main - entry point 
- *
  * print_alphabet - print alphabet lowercase
- * Return: always 0
  *
+ * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-	char alpabet;
+char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-		_putchar(alphabet);
-	}
-	_putchar('\n');
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
 }
